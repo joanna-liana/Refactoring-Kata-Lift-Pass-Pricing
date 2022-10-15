354856eadadbbe1ed26b60a323d75f290a60fe42
Joanna Liana <joanna_liana@outlook.com>
2022-10-15 08:41:55 +0200

    Test cost for ages under threshold

--- test/prices.spec.ts
+++ test/prices.spec.ts
@@ -1,26 +1,35 @@
-import {assert, expect} from 'chai';
+import { assert, expect } from 'chai';
 import request from 'supertest-as-promised';
-import {createApp} from "../src/prices"
+import { createApp } from "../src/prices";
 
 describe('prices', () => {
 
-    let app, connection
+  let app, connection;
 
-    beforeEach(async () => {
-        ({app, connection} = await createApp());
-    });
+  beforeEach(async () => {
+    ({ app, connection } = await createApp());
+  });
 
-    afterEach(async () => {
-        await connection.end()
-    });
+  afterEach(async () => {
+    await connection.end();
+  });
 
-    it('does something', async () => {
+  it('does something', async () => {
 
-        const response = await request(app)
-            .get('/prices?type=1jour')
+    const response = await request(app)
+      .get('/prices?type=1jour');
 
-        var expectedResult = {cost: 35} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-    });
+    var expectedResult = { cost: 35 }; // change this to make the test pass
+    expect(response.body).deep.equal(expectedResult);
+  });
 
+  it("given age below threshold, the cost is 0", async () => {
+    const ANY_TYPE = '1jour';
+    const response = await request(app)
+      .get(`/prices?type=${ANY_TYPE}&age=5`);
+
+    var expectedResult = { cost: 0 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
 });
